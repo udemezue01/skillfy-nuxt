14c93a6df0e6b6f2caf6930f8f3890290a3d9393
Udemezue John <johnzeus14@gmail.com>
2022-12-28 07:48:02 +0100

    Updated footer, Hero and navbar section

--- components/hero.vue
+++ components/hero.vue
@@ -1,12 +1,19 @@
-<!-- Please remove this file from your project -->
 <template>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-
-
-
+  <section class="text-gray-600 font-body">
+<div class="container m-auto flex px-5 py-24 items-center justify-center flex-col">
+  
+  <div class="text-center lg:w-2/3 w-full space-y-10">
+    <h1 class="title-font text-3xl xl:text-5xl font-bold text-gray-900">The best remote jobs all over the internet</h1>
+    <p class="mb-8 leading-relaxed text-2xl">Skillfy is the #1 place to find remote job. Find your perfect remote job today .</p>
+    <div class="flex justify-center">
+      <button class="inline-flex text-white bg-purple-500 border-0 py-2 px-6 focus:outline-none hover:bg-purple-600 rounded text-lg">Get Started</button>
+      <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Join</button>
     </div>
-      
-  </template>
+  </div>
+</div>
+</section>
+
+</template>
   
   <script>
   export default {
