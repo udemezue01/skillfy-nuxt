383d1c5efafe3e482229634794e93f91b2477ebf
Udemezue John <johnzeus14@gmail.com>
2022-12-29 11:27:13 +0100

    updated dashboard

--- store/modules/auth.js
+++ store/modules/auth.js
@@ -1,28 +1,63 @@
 
 
+const state =  {
 
-const state = {
-
-
-
-}
-
-const mutations = {
-
-
-}
-
-const getters = {
-
+    jobs:[],
+    filteredJobs:[],
     
-}
-
-const actions = {
-
-
-
-}
-
-
-
-
+    };
+    
+    
+    const getters = {
+    
+    
+    
+    };
+    
+    
+    const mutations = {
+    
+    SET_JOBS(state, news){
+    
+    state.news = news 
+    },
+    
+    
+    
+    };
+    
+    const actions = {
+    
+    // The News list API 
+    
+    async jobCreate({commit}){
+    
+    try{
+    
+    
+    const response = await this.$axios.$get(endPoint, config)
+    const news  	= response.articles
+    
+    commit('SET_NEWS', news)
+    
+    
+    }
+    catch(e){
+    
+    console.log(e)
+    }
+    
+    },
+    
+    
+    
+    };
+    
+    
+    export default {
+    
+    state,
+    getters,
+    mutations,
+    actions,
+    }
--- store/modules/jobs.js
+++ store/modules/jobs.js
@@ -1,28 +1,63 @@
 
 
+const state =  {
 
-const state = {
+jobs:[],
+filteredJobs:[],
 
+};
 
-
-}
-
-const mutations = {
-
-
-}
 
 const getters = {
 
-    
-}
+
+
+};
+
+
+const mutations = {
+
+SET_JOBS(state, jobs){
+
+state.jobs = jobs 
+},
+
+
+
+};
 
 const actions = {
 
+// The News list API 
+
+async jobCreate({commit}){
+
+try{
+
+
+const response = await this.$axios.$get(endPoint, config)
+const news  	= response.articles
+
+commit('SET_JOBS', news)
 
 
 }
+catch(e){
+
+console.log(e)
+}
+
+},
 
 
 
+};
 
+
+export default {
+
+state,
+getters,
+mutations,
+actions,
+}
